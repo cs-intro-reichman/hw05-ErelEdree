576d30947524c037a1a9a85cdc8dda069866bd7c
Erel Edree <ereledreeuk@gmail.com>
2024-01-18 20:10:36 +0200

    added spaces in print

--- GameOfLife.java
+++ GameOfLife.java
@@ -159,7 +159,7 @@ public class GameOfLife {
 		//// Write your code here.
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[0].length; j++) {
-				System.out.print(arr[i][j]);
+				System.out.print(arr[i][j] + " ");
 			}
 			System.out.println();
 		}
